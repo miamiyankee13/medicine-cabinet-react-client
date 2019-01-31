2d7dea62a4388cba7156f0fcf05b438ad106b963
Anthony Damico <anthony.damico721@gmail.com>
2019-01-31 09:08:21 -0500

    set up initial actions, reducer, & store

---
+++ src/store.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import thunk from 'redux-thunk';
+import { loadAuthToken } from './local-storage';
+import authReducer from './reducers/auth';
+import { setAuthToken, refreshAuthToken } from './actions/auth';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    combineReducers({
+        form: formReducer,
+        auth: authReducer
+    }),
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+);
+
+// Hydrate the authToken from localStorage if it exist
+const authToken = loadAuthToken();
+if (authToken) {
+    const token = authToken;
+    store.dispatch(setAuthToken(token));
+    store.dispatch(refreshAuthToken());
+}
+
+export default store;
