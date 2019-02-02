00f7f9722f5a1932bb1a1e764e79dd3a7f08733c
Anthony Damico <anthony.damico721@gmail.com>
2019-02-02 14:50:33 -0500

    created custom form for create strain

--- src/components/create-strain-form.js
+++ src/components/create-strain-form.js
@@ -1,37 +1,109 @@
 import React from 'react';
-import { Field, reduxForm, reset } from 'redux-form';
 import { createStrain } from '../actions/strain-data';
-import Input from './input';
-import { required, nonEmpty} from '../validators';
+import { connect } from 'react-redux';
 
 export class CreateStrainForm extends React.Component {
-    onSubmit(values) {
-        const {name, type, description, flavor} = values;
-        return this.props.dispatch(createStrain(name, type, description, flavor))
-            .then(() => this.props.dispatch(reset('createStrain')))
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameValue: '',
+            typeValue: '',
+            descriptionValue: '',
+            flavorValue: ''
+        };
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleTypeChange = this.handleTypeChange.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handleFlavorChange = this.handleFlavorChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    
+    handleNameChange(event) {
+        this.setState({
+            nameValue: event.target.value
+        });
+    }
+
+    handleTypeChange(event) {
+        this.setState({
+            typeValue: event.target.value
+        });
+    }
+
+    handleDescriptionChange(event) {
+        this.setState({
+            descriptionValue: event.target.value
+        });
+    }
+
+    handleFlavorChange(event) {
+        this.setState({
+            flavorValue: event.target.value
+        });
+    }
+    
+    onSubmit(event) {
+        event.preventDefault();
+        const name = this.state.nameValue;
+        const type = this.state.typeValue;
+        const description = this.state.descriptionValue;
+        const flavor = this.state.flavorValue;
+        this.props.dispatch(createStrain(name, type, flavor, description))
+            .then(() => this.setState({
+                nameValue: '',
+                typeValue: '',
+                descriptionValue: '',
+                flavorValue: ''
+            }))
     }
 
     render() {
         return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
+            <form className="create-strain-form" onSubmit={this.onSubmit}>
                 <label htmlFor="name">Name</label>
-                <Field component={Input} type="text" name="name" validate={[required, nonEmpty]} />
+                <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    value={this.state.nameValue}
+                    onChange={this.handleNameChange}
+                    required/>
+           
                 <label htmlFor="type">Type</label>
-                <Field component={Input} type="text" name="type" validate={[required, nonEmpty]} />
+                <input 
+                    type="text" 
+                    id="type" 
+                    name="type" 
+                    value={this.state.typeValue}
+                    onChange={this.handleTypeChange}
+                    required/>
+                
                 <label htmlFor="description">Description</label>
-                <Field component={Input} type="text" name="description" validate={[required, nonEmpty]} />
+                <textarea 
+                    id="strain-description-edit" 
+                    name="strain-description-edit" 
+                    rows="4" 
+                    cols="50"
+                    value={this.state.descriptionValue}
+                    onChange={this.handleDescriptionChange}
+                    required
+                    >
+                </textarea>
+
                 <label htmlFor="flavor">Flavor</label>
-                <Field component={Input} type="text" name="flavor" validate={[required, nonEmpty]} />
-                <button type="submit" disabled={this.props.pristine || this.props.submitting}>
-                    Create
-                </button>
+                <input 
+                    type="text" 
+                    id="flavor" 
+                    name="flavor" 
+                    value={this.state.flavorValue}
+                    onChange={this.handleFlavorChange}
+                    required/>
+
+                <button type="submit">Create Strain</button>
             </form>
         );
     }
 }
 
-export default reduxForm({
-    form: 'createStrain',
-})(CreateStrainForm);
+export default connect()(CreateStrainForm);
