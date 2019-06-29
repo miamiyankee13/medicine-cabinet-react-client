02b3ec81cb0b9433d96de2606e93004f9b59eb9e
Anthony Damico <anthony.damico721@gmail.com>
2019-06-29 09:23:32 -0400

    setup initial routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './containers/Layout/Layout';
 
 const App = () => (
     <Fragment>
         <Layout>
-            Content
+            <Switch>
+                <Route path="/cabinet" render={() => <h1>Cabinet</h1>} />
+                <Route path="/add" render={() => <h1>Add Strain</h1>} />
+                <Route path="/edit" render={() => <h1>Edit Strain</h1>} />
+                <Route path="/" render={() => <h1>Landing Page</h1>} />
+            </Switch>
         </Layout>
     </Fragment>
 );
