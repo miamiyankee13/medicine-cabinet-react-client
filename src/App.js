3b12a82392613496729603bc1553135cb2c7bc91
Anthony Damico <anthony.damico721@gmail.com>
2019-07-03 08:48:46 -0400

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Layout from './containers/Layout/Layout';
+import AddForm from './containers/Forms/AddForm/AddForm';
+import EditForm from './containers/Forms/EditForm/EditForm';
 
 const App = () => (
     <Fragment>
         <Layout>
             <Switch>
                 <Route path="/cabinet" render={() => <h1>Cabinet</h1>} />
-                <Route path="/add" render={() => <h1>Add Strain</h1>} />
-                <Route path="/edit" render={() => <h1>Edit Strain</h1>} />
+                <Route path="/add" component={AddForm} />
+                <Route path="/edit" component={EditForm} />
                 <Route path="/" render={() => <h1>Landing Page</h1>} />
             </Switch>
         </Layout>
