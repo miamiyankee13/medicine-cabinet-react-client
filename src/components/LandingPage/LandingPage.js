a00d4f9c8d0b653d40927729394e22b9df505b3b
Anthony Damico <anthony.damico721@gmail.com>
2019-07-07 08:00:54 -0400

    added warning

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -20,16 +20,17 @@ class LandingPage extends Component {
 
         return (
             <section className={styles.home} aria-live="polite">
-                <p>
+                <p className={styles.intro}>
                     Medicine Cabinet offers a simple, unique way to journal about & 
                     keep track of medicinal cannabis strains.
                 </p>
-                <p>
+                <p className={styles.intro}>
                     What makes Medicine Cabinet special is the 
                     community aspect. The more each user shares the various experiences they have had with 
                     specific strains, the more information will be available to medical patients all over 
                     the world!
                 </p>
+                <p><strong>FOR USE BY ADULTS ONLY - AGES 21 & OLDER</strong></p>
                 <div className={styles.buttons}>
                     <Button clicked={this.handleLogin}>Log In</Button>
                     <Button clicked={this.handleRegistration}>Register</Button>
