6ab2a26bc159246db8af45c83693cb48ec631237
Anthony Damico <anthony.damico721@gmail.com>
2019-07-23 08:16:28 -0400

    added more tests

--- src/actions/test/strains.test.js
+++ src/actions/test/strains.test.js
@@ -13,6 +13,9 @@ import {
     FETCH_CURRENT_STRAIN_SUCCESS,
     FETCH_CURRENT_STRAIN_ERROR,
     RESET_CURRENT_STRAIN,
+    CREATE_STRAIN_REQUEST,
+    CREATE_STRAIN_SUCCESS,
+    CREATE_STRAIN_ERROR,
     fetchStrainsRequest,
     fetchStrainsSuccess,
     fetchStrainsError,
@@ -25,7 +28,10 @@ import {
     fetchCurrentStrainRequest,
     fetchCurrentStrainSuccess,
     fetchCurrentStrainError,
-    resetCurrentStrain
+    resetCurrentStrain,
+    createStrainRequest,
+    createStrainSuccess,
+    createStrainError
 } from '../strains';
 
 describe('Action Creators', function() {
@@ -140,6 +146,29 @@ describe('Action Creators', function() {
         it('Should return the action', function() {
             const action = resetCurrentStrain();
             expect(action.type).toEqual(RESET_CURRENT_STRAIN);
+        });
+    });
+
+    describe('createStrainRequest', function() {
+        it('Should return the action', function() {
+            const action = createStrainRequest();
+            expect(action.type).toEqual(CREATE_STRAIN_REQUEST);
+        });
+    });
+
+    describe('createStrainSuccess', function() {
+        it('Should return the action', function() {
+            const action = createStrainSuccess();
+            expect(action.type).toEqual(CREATE_STRAIN_SUCCESS);
+        });
+    });
+
+    describe('createStrainError', function() {
+        it('Should return the action', function() {
+            const error = mockData.error;
+            const action = createStrainError(error);
+            expect(action.type).toEqual(CREATE_STRAIN_ERROR);
+            expect(action.error).toEqual(error);
         });
     });
 })
